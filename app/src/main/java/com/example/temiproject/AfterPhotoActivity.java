687a00cbd9761998d3590eb6c12acffc7cbb60ea
vianshang <stu0417182729@gmail.com>
2020-08-31 14:58:35 +0800

    Camera final version

--- app/src/main/java/com/example/temiproject/AfterPhotoActivity.java
+++ app/src/main/java/com/example/temiproject/AfterPhotoActivity.java
@@ -50,6 +50,11 @@ public class AfterPhotoActivity extends ActivityController {
     ImageView ivQRcode;
     final String TAG = "AfterPhotoActivity";
     Button btQRcode;
+    Button frame1_btn;
+    Button frame2_btn;
+    Button frame3_btn;
+    Button frame4_btn;
+    Button frame5_btn;
     private ProgressBar spinner;
 
 
@@ -57,12 +62,12 @@ public class AfterPhotoActivity extends ActivityController {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_photo);
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
         spinner.setVisibility(View.GONE);
 
         findView();
         addListener();
-        final Button home_btn = (Button)findViewById(R.id.home_btn);
+        final Button home_btn = (Button) findViewById(R.id.home_btn);
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,7 +80,9 @@ public class AfterPhotoActivity extends ActivityController {
                 toNextActivity();
             }
         });
-
+    }
+    protected void onStart(){
+        super.onStart();
         String path = getIntent().getStringExtra("picpath");//通過值"picpath"得到照片路徑
         final ImageView imageview = findViewById(R.id.preview_img);
         try{
@@ -98,12 +105,17 @@ public class AfterPhotoActivity extends ActivityController {
 
         Drawable frame1 = new BitmapDrawable(bitmap);
         imageview.setImageDrawable(frame1);
-        final Button frame1_btn = (Button)findViewById(R.id.frame1_btn);
-        final Button frame2_btn = (Button)findViewById(R.id.frame2_btn);
-        final Button frame3_btn = (Button)findViewById(R.id.frame3_btn);
-        final Button frame4_btn = (Button)findViewById(R.id.frame4_btn);
-        final Button frame5_btn = (Button)findViewById(R.id.frame5_btn);
-
+        frame1_btn = (Button)findViewById(R.id.frame1_btn);
+        frame2_btn = (Button)findViewById(R.id.frame2_btn);
+        frame3_btn = (Button)findViewById(R.id.frame3_btn);
+        frame4_btn = (Button)findViewById(R.id.frame4_btn);
+        frame5_btn = (Button)findViewById(R.id.frame5_btn);
+        btQRcode.setVisibility(View.VISIBLE);
+        frame1_btn.setVisibility(View.VISIBLE);
+        frame2_btn.setVisibility(View.VISIBLE);
+        frame3_btn.setVisibility(View.VISIBLE);
+        frame4_btn.setVisibility(View.VISIBLE);
+        frame5_btn.setVisibility(View.VISIBLE);
 
         Drawable photoplus = combineGraph(frame1, frame1);
         photo = ((BitmapDrawable)photoplus).getBitmap();
@@ -111,8 +123,7 @@ public class AfterPhotoActivity extends ActivityController {
         frame1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation bounce = AnimationUtils.loadAnimation(AfterPhotoActivity.this, R.anim.bounce_animation);
-                frame1_btn.startAnimation(bounce);
+
                 Drawable frame1 = new BitmapDrawable(bitmap);
                 frame1 = combineGraph(frame1, frame1);
                 imageview.setImageDrawable(frame1);
@@ -124,8 +135,7 @@ public class AfterPhotoActivity extends ActivityController {
         frame2_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation bounce = AnimationUtils.loadAnimation(AfterPhotoActivity.this, R.anim.bounce_animation);
-                frame2_btn.startAnimation(bounce);
+
                 Drawable frame1 = new BitmapDrawable(bitmap);
                 Drawable frame2 = getDrawable(R.drawable.frame_1);
                 frame2 = combineGraph(frame2, frame1);
@@ -138,8 +148,7 @@ public class AfterPhotoActivity extends ActivityController {
         frame3_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation bounce = AnimationUtils.loadAnimation(AfterPhotoActivity.this, R.anim.bounce_animation);
-                frame3_btn.startAnimation(bounce);
+
                 Drawable frame1 = new BitmapDrawable(bitmap);
                 Drawable frame3 = getDrawable(R.drawable.frame_2);
                 frame3 = combineGraph(frame3, frame1);
@@ -152,8 +161,7 @@ public class AfterPhotoActivity extends ActivityController {
         frame4_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation bounce = AnimationUtils.loadAnimation(AfterPhotoActivity.this, R.anim.bounce_animation);
-                frame4_btn.startAnimation(bounce);
+
                 Drawable frame1 = new BitmapDrawable(bitmap);
                 Drawable frame4 = getDrawable(R.drawable.frame_3);
                 frame4 = combineGraph(frame4, frame1);
@@ -166,8 +174,7 @@ public class AfterPhotoActivity extends ActivityController {
         frame5_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation bounce = AnimationUtils.loadAnimation(AfterPhotoActivity.this, R.anim.bounce_animation);
-                frame5_btn.startAnimation(bounce);
+
                 Drawable frame1 = new BitmapDrawable(bitmap);
                 Drawable frame5 = getDrawable(R.drawable.frame_4);
                 frame5 = combineGraph(frame5, frame1);
@@ -222,24 +229,14 @@ public class AfterPhotoActivity extends ActivityController {
             public void onClick(View view) {
                 Log.d(TAG, "onClick: btQRcode");
                 Toast.makeText(AfterPhotoActivity.this, "sent", Toast.LENGTH_SHORT).show();
-
+                speak("資料傳輸中 請稍候");
                 //make other button disappear
-
-
-
-                Button frame1_btn = (Button)findViewById(R.id.frame1_btn);
-                Button frame2_btn = (Button)findViewById(R.id.frame2_btn);
-                Button frame3_btn = (Button)findViewById(R.id.frame3_btn);
-                Button frame4_btn = (Button)findViewById(R.id.frame4_btn);
-                Button frame5_btn = (Button)findViewById(R.id.frame5_btn);
-                frame1_btn.setVisibility(View.GONE);
-                frame2_btn.setVisibility(View.GONE);
-                frame3_btn.setVisibility(View.GONE);
-                frame4_btn.setVisibility(View.GONE);
-                frame5_btn.setVisibility(View.GONE);
-                btQRcode.setVisibility(View.GONE);
-
-                spinner.bringToFront();//用了還是會被蓋住
+                btQRcode.setVisibility(View.INVISIBLE);
+                frame1_btn.setVisibility(View.INVISIBLE);
+                frame2_btn.setVisibility(View.INVISIBLE);
+                frame3_btn.setVisibility(View.INVISIBLE);
+                frame4_btn.setVisibility(View.INVISIBLE);
+                frame5_btn.setVisibility(View.INVISIBLE);
                 spinner.setVisibility(View.VISIBLE);
 
 
@@ -327,6 +324,8 @@ public class AfterPhotoActivity extends ActivityController {
                 } catch (WriterException e) {
                     e.printStackTrace();
                 }
+
+                speak("請掃QR code 取回您的照片");
 
 
 
--- app/src/main/java/com/example/temiproject/InformationActivity.java
+++ app/src/main/java/com/example/temiproject/InformationActivity.java
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
 import java.util.TimerTask;
 
 public class InformationActivity extends ActivityController {
 
-
-    java.util.Timer timer;
+    count count;
+    Timer timer;
     char next_job = ' ';
 
-    TimerTask count = new TimerTask() {
+    class count extends TimerTask{
         public void run() {
             Intent intent2 = new Intent(InformationActivity.this, MovingActivity.class);
             intent2.putExtra("task", "picture");
@@ -26,14 +27,18 @@ public class InformationActivity extends ActivityController {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
-
+        speak("將帶領您至拍照地點");
+        timer = new Timer();
+        count = new count();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         cancelTimer();
-        timer = new java.util.Timer(true);
+        count.cancel();
+        timer = new Timer();
+        count = new count();
         timer.schedule(count, 3000);
     }
 
